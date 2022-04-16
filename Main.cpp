a262e4c802a0e15a42165b549ffb6325379312d0
Musk Modder <85071855+springmusk026@users.noreply.github.com>
2022-04-16 10:42:52 +0545

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,46 @@
+#include "EntityList.h"
+
+/*HOOK CLASS VEHICLE*\
+void (*orig_VehicleUpdate)(void *componentPlayer);
+void _VehicleUpdate(void *player) {
+  
+  ///direct implementation  may cause crash so use it by making proper conditions
+  vehicleManager->tryAddEnemy(player);
+  vehicleManager->updateEnemies(player);
+  
+  orig_VehicleUpdate(player);
+}
+
+
+/////put this inside your esp class
+
+ if (vehicleManager->enemies->empty()) {
+                return;
+            }
+            for (int i = 0; i < vehicleManager->enemies->size(); i++) {
+                void *Vehicle = (*vehicleManager->enemies)[i]->object;
+                
+                Vector3 VehiclePos =Transform_INTERNAL_GetPosition(get_Transform(Vehicle));
+                void *Cam = Camera_main();
+                Vector3 PosNew = WorldToScreenPoint(Cam, VehiclePos);
+                if (PosNew.Z < 1.f) continue;
+                float distance = Vector3::Distance(GetAttackableCenterWS(Current_Local_Player()), VehiclePos);
+
+                if(distance< configs.esp.vMinDist){
+                    if(configs.esp.vLine){
+                        esp.DrawLine(Color::White(), 0.5f, Vector3((screenWidth / 2), screenHeight/20),  Vector3((screenWidth - (screenWidth - PosNew.X)), (screenHeight -PosNew.Y)));
+                    }
+                    if(configs.esp.vDist){
+                        char buffer[30];
+                        sprintf(buffer, "Vehicle : [ %.2fm ]", distance);
+                        esp.DrawText(Color::White(), buffer, Vector3((screenWidth - (screenWidth - PosNew.X)), (screenHeight -PosNew.Y)), 10.0f);
+                    }
+                }
+            }
+            
+            
+            
+/////Put this on your hack thread
+
+HOOK(string2Offset(OBFUSCATE_KEY("0x10100C8", 'k')), _VehicleUpdate, orig_VehicleUpdate);
+            
